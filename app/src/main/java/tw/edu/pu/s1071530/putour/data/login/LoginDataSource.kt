67781995e2ad8cc95186f9fc2bc10b31400a84bf
月太げつたい <me@moontai0724.tw>
2021-05-05 16:05:37 +0800

    feat: add login account

--- app/src/main/java/tw/edu/pu/s1071530/putour/data/login/LoginDataSource.kt
+++ app/src/main/java/tw/edu/pu/s1071530/putour/data/login/LoginDataSource.kt
@@ -6,9 +6,10 @@ import java.io.IOException
 class LoginDataSource {
     fun login(username: String, password: String): Result<User> {
         return try {
-            if (username != "puapp" || password != "94168")
+            if ((username == "puapp" && password == "94168") || (username == "puuser" && password == "54168"))
+                Result.Success(User(username, password))
+            else
                 throw Error("Invalid user")
-            Result.Success(User(username, password))
         } catch (e: Throwable) {
             Result.Error(IOException("Error logging in", e))
         }
